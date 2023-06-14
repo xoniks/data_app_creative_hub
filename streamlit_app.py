cc564b3afb728638205dbc0f67718f110b831890
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-14 19:53:45 +0200

    before_deploymnet

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 df = None
 
-st.title('Creative hub data science projec.subheader')
+st.title('Creative hub data science project ')
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file) 
