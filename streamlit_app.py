c9785d5f63b4e06825c4cbd2d0747cb47ba7319d
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-14 18:54:32 +0200

    dict added to options

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,12 @@ import streamlit as st
 df = None
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write('File uploaded')
-  list_of_countries = df['country'].unique()
+if uploaded_file is not None:    
+    df = pd.read_csv(uploaded_file)
+    st.write('File uploaded')
+    list_of_countries = df['country'].unique()
+    country_value_counts = df['country'].value_counts()
+    count_country_dict = country_value_counts.to_dict()
   
 
 
@@ -23,5 +25,6 @@ option = st.selectbox(
     list_of_countries)
 
 if st.button("Show how many times you country occurs in dataframe"):
-    num = len(df[df['country']==option])
+    # num = len(df[df['country']==option])
+    num = count_country_dict[option]
     st.write(f'Your {option} occurs {num} times')
