a04b07fcf39262124771724fc597d11dd9009e21
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-14 19:18:49 +0200

    albans code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,19 @@ import streamlit as st
 df = None
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:    
-    df = pd.read_csv(uploaded_file)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file) 
     st.write('File uploaded')
+    
+    
+if df is None:
+    st.write('Upload the dataframe')   
+else:
     list_of_countries = df['country'].unique()
     country_value_counts = df['country'].value_counts()
     count_country_dict = country_value_counts.to_dict()
-  
-
-
-# if st.button('Show list of countries'):
-#     if df is None:
-#         st.write('Upload the dataframe')
-#     else:
-#         list_of_countries = df['country'].unique()
-        # st.write(list_of_countries)
-        
-option = st.selectbox(
-    'Choose your country:',
-    list_of_countries)
+    st.bar_chart(country_value_counts)        
+    option = st.selectbox('Choose your country:',  list_of_countries)
 
 if st.button("Show how many times you country occurs in dataframe"):
     # num = len(df[df['country']==option])
