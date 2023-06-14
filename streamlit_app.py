4790f3b23c63a127cc353a1283be26b6fac5b371
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-14 18:45:10 +0200

    option added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,27 @@
 import pandas as pd
 import streamlit as st
 
-# df = None
+df = None
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write('File uploaded')
-#   st.write(df)
+  list_of_countries = df['country'].unique()
   
-# df = pd.read_csv('sample_data.csv')
 
 
+# if st.button('Show list of countries'):
+#     if df is None:
+#         st.write('Upload the dataframe')
+#     else:
+#         list_of_countries = df['country'].unique()
+        # st.write(list_of_countries)
+        
+option = st.selectbox(
+    'Choose your country:',
+    list_of_countries)
 
-if st.button('Show list of countries'):
-    if df is None:
-        st.write('Upload the dataframe')
-    else:
-        list_of_countries = df['country'].unique()
-        st.write(list_of_countries)
+if st.button("Show how many times you country occurs in dataframe"):
+    num = len(df[df['country']==option])
+    st.write(f'Your {option} occurs {num} times')
