c34bb66c9ce601acb6cdb6b2ecc57fdd13833323
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-06-14 18:25:29 +0200

    file upload added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('sample_data.csv')
+# df = None
 
-list_of_countries = df['country'].unique()
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write('File uploaded')
+#   st.write(df)
+  
+# df = pd.read_csv('sample_data.csv')
+
+
 
 if st.button('Show list of countries'):
-    st.write(list_of_countries)
+    if df is None:
+        st.write('Upload the dataframe')
+    else:
+        list_of_countries = df['country'].unique()
+        st.write(list_of_countries)
